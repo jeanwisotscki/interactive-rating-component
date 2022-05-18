72a55fea49863209aefb3cd8035e492c98709320
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-05-18 09:13:06 -0300

    inicio projeto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import "./App.css";
+import Box from "./Components/Box";
 
 const App = () => {
   return (
     <div>
       <h1>oi teste</h1>
+      <Box />
     </div>
   );
 };
---
+++ src/Components/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Box.module.css";
+
+const Box = () => {
+  return (
+    <div>
+      <h1 className={styles.teste}>interactive</h1>
+    </div>
+  );
+};
+
+export default Box;
