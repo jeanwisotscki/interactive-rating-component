4aa36bbe8e25ff4b09c33e941f87c226cbec31ea
Jean Wisotscki <jean1wisotscki@gmail.com>
2022-05-18 10:14:45 -0300

    style pré finalizado

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Box from "./Components/Box";
 
 const App = () => {
   return (
-    <div>
-      <h1>oi teste</h1>
+    <div className="App">
       <Box />
     </div>
   );
--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,10 +1,30 @@
 import React from "react";
 import styles from "./Box.module.css";
+import iconStar from "../images/icon-star.svg";
 
 const Box = () => {
   return (
-    <div>
-      <h1 className={styles.teste}>interactive</h1>
+    <div className={styles.box}>
+      <div className={styles.circle}>
+        <img src={iconStar} alt="icon star" />
+      </div>
+      <div className={styles.textWrapper}>
+        <h1>How did we do?</h1>
+        <p>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve uor offering!
+        </p>
+      </div>
+      <div>
+        <ul className={styles.rating}>
+          <li className={styles.circle}>1</li>
+          <li className={styles.circle}>2</li>
+          <li className={styles.circle}>3</li>
+          <li className={styles.circle}>4</li>
+          <li className={styles.circle}>5</li>
+        </ul>
+      </div>
+      <button className={styles.btn}>SUBMIT</button>
     </div>
   );
 };
